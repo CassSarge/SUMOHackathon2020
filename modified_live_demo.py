9f8e34a07dcdb045242706ba02fad16e13678818
NickSarge <nicholassargeant@gmail.com>
2020-10-08 15:23:18 +1100

    Added demo and test mode
    Demo mode for checking single alphabets, testing mode for seeing prediction results

--- modified_live_demo.py
+++ modified_live_demo.py
@@ -15,43 +15,83 @@ from model import create_model
 import argparse
 import numpy as np
 
-def testing_mode(predictions, frame, width, height):
+
+def testing_mode(my_predict, frame, width, height, label_dict):
     ''' Will display the top three predictions for the ASL symbol in the ROI'''
 
-    (prediction_result, pred_2, pred_3) = predictions
-    # Annotate image with most probable prediction
-    cv2.putText(frame, text=prediction_result,
-                org=(width // 2, height // 2),
-                fontFace=cv2.FONT_HERSHEY_COMPLEX,
-                fontScale=17, color=(213,152,20),
-                thickness=15, lineType=cv2.LINE_AA)
-    # Annotate image with second most probable prediction (displayed on bottom left)
-    cv2.putText(frame, text=pred_2,
-                org=(width // 2 + width // 5, (360 + 240)),
-                fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                fontScale=6, color=(97, 140, 94),
-                thickness=6, lineType=cv2.LINE_AA)
-    # Annotate image with third probable prediction (displayed on bottom right)
-    cv2.putText(frame, text=pred_3,
-                org=(width // 2 + width // 3 + 5, (360 + 240)),
-                fontFace=cv2.FONT_HERSHEY_DUPLEX,
-                fontScale=6, color=(97,140,94),
-                thickness=6, lineType=cv2.LINE_AA)
+    # Predict letter
+    top_prd = np.argmax(my_predict)
+
+    # Only display predictions with probabilities greater than 0.5
+    if np.max(my_predict) >= 0.50:
+
+        prediction_result = label_dict[top_prd]
+
+        # sorts list in asceding order - therefore to get the 2nd and 3rd most likely option, we pick index -2 and -3
+        preds_list = np.argsort(my_predict)[0]
+        pred_2 = label_dict[preds_list[-2]]
+        pred_3 = label_dict[preds_list[-3]]
+
+        # Annotate image with most probable prediction
+        cv2.putText(frame, text=prediction_result,
+                    org=(width // 2, height // 2),
+                    fontFace=cv2.FONT_HERSHEY_COMPLEX,
+                    fontScale=17, color=(213,152,20),
+                    thickness=15, lineType=cv2.LINE_AA)
+        # Annotate image with second most probable prediction (displayed on bottom left)
+        cv2.putText(frame, text=pred_2,
+                    org=(width // 2 + width // 5, (360 + 240)),
+                    fontFace=cv2.FONT_HERSHEY_DUPLEX,
+                    fontScale=6, color=(97, 140, 94),
+                    thickness=6, lineType=cv2.LINE_AA)
+        # Annotate image with third probable prediction (displayed on bottom right)
+        cv2.putText(frame, text=pred_3,
+                    org=(width // 2 + width // 3 + 5, (360 + 240)),
+                    fontFace=cv2.FONT_HERSHEY_DUPLEX,
+                    fontScale=6, color=(97,140,94),
+                    thickness=6, lineType=cv2.LINE_AA)
 
 
 
-def demo_mode(target, predictions, frame, width, height):
+def demo_mode(target, my_predict, frame, width, height, label_dict):
 
     ''' Will display the target letter and respond with a tick, 
     self closes when the target letter is detected for a time period'''
-
-    # (prediction_result, pred_2, pred_3) = predictions
 
     cv2.putText(frame, text=target,
                 org=(width // 2, height // 2),
                 fontFace=cv2.FONT_HERSHEY_COMPLEX,
                 fontScale=17, color=(213,152,20),
                 thickness=15, lineType=cv2.LINE_AA)
+
+        # Predict letter
+    top_prd = np.argmax(my_predict)
+    prediction_result = label_dict[top_prd]
+
+    # Confident we at least have a hand
+    if np.max(my_predict) >= 0.50:
+        # Annotate image in bottom left
+        cv2.putText(frame, text=prediction_result,
+                    org=(width // 2 + width // 5, (360 + 240)),
+                    fontFace=cv2.FONT_HERSHEY_DUPLEX,
+                    fontScale=6, color=(97, 140, 94),
+                    thickness=6, lineType=cv2.LINE_AA)
+
+        preds_list = np.argsort(my_predict)[0]
+        pred_2 = label_dict[preds_list[-2]]
+        pred_3 = label_dict[preds_list[-3]]
+
+        # Display success only if target is in top 3 prediction results
+        if target in (prediction_result, pred_2, pred_3):
+
+            # Annotate image with success message
+            cv2.putText(frame, text="Success!!",
+                org=(width // 2 + width // 3 + 5, (360 + 240)),
+                fontFace=cv2.FONT_HERSHEY_DUPLEX,
+                fontScale=6, color=(97, 140, 94),
+                thickness=6, lineType=cv2.LINE_AA)
+
+    
 
 def main():
     ap = argparse.ArgumentParser()
@@ -68,7 +108,7 @@ def main():
         mode = mydict["TESTING"]
     else:
         mode = mydict["DEMO"]
-        targetLetter = args['target']
+        targetLetter = args['target'].upper()
 
 
 
@@ -103,6 +143,10 @@ def main():
     fps = 0
     start = time.time()
 
+    width = int(video_capture.get(3) + 0.5)
+    height = int(video_capture.get(4) + 0.5)
+
+
     while True:
         # Capture frame-by-frame
         _, frame = video_capture.read()
@@ -127,50 +171,10 @@ def main():
                                     batch_size=1,
                                     verbose=0)
 
-        # Predict letter
-        top_prd = np.argmax(my_predict)
-
-        # Only display predictions with probabilities greater than 0.5
-        if np.max(my_predict) >= 0.50:
-
-            prediction_result = label_dict[top_prd]
-
-            # sorts list in asceding order - therefore to get the 2nd and 3rd most likely option, we pick index -2 and -3
-            preds_list = np.argsort(my_predict)[0]
-            pred_2 = label_dict[preds_list[-2]]
-            pred_3 = label_dict[preds_list[-3]]
-
-            predictions = (prediction_result, pred_2, pred_3)
-
-            width = int(video_capture.get(3) + 0.5)
-            height = int(video_capture.get(4) + 0.5)
-
-            if (mode):
-                testing_mode(predictions, frame, width, height)
-            else:
-                demo_mode(targetLetter, predictions, frame, width, height)
-
-
-
-
-            # # Annotate image with most probable prediction
-            # cv2.putText(frame, text=prediction_result,
-            #             org=(width // 2 + 230, height // 2 + 75),
-            #             fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-            #             fontScale=17, color=(255, 255, 0),
-            #             thickness=15, lineType=cv2.LINE_AA)
-            # # Annotate image with second most probable prediction (displayed on bottom left)
-            # cv2.putText(frame, text=pred_2,
-            #             org=(width // 2 + width // 5 + 40, (360 + 240)),
-            #             fontFace=cv2.FONT_HERSHEY_PLAIN,
-            #             fontScale=6, color=(0, 0, 255),
-            #             thickness=6, lineType=cv2.LINE_AA)
-            # # Annotate image with third probable prediction (displayed on bottom right)
-            # cv2.putText(frame, text=pred_3,
-            #             org=(width // 2 + width // 3 + 5, (360 + 240)),
-            #             fontFace=cv2.FONT_HERSHEY_PLAIN,
-            #             fontScale=6, color=(0, 0, 255),
-            #             thickness=6, lineType=cv2.LINE_AA)
+        if (mode):
+            testing_mode(my_predict, frame, width, height, label_dict)
+        else:
+            demo_mode(targetLetter, my_predict, frame, width, height, label_dict)
 
         # Display the resulting frame
         cv2.imshow('Video', frame)
